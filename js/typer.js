cc6c87eeb357ba0377ae1a8eb4448a5b6ad9d320
sidaga kihongo <sidagawazirikihongo@gmail.com>
2024-05-16 18:39:12 +0300

    finalizing project

--- js/typer.js
+++ js/typer.js
@@ -2,9 +2,13 @@ document.addEventListener("DOMContentLoaded", function(){
     var typed = new Typed("#typed-output",{
         strings:["Welcome to the Convocation Ceremony at EASTC"],
         typeSpeed: 100,
+        
         backSpeed: 50,
-        backDelay:2000,
-        startDelay:500,        
-        loop:true
+        backDelay: 2000,
+        startDelay: 500,
+        loop: true,
+        showCursor: false,//cursor blinking
+        autoResize: false, // Keep container size constant
+
     });
 });
